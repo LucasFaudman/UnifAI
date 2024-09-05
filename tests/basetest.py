4802075d376029147945811d1b421d484a1c4706
Lucas Faudman <Lucasfaudman@gmail.com>
2024-09-05 02:28:02 -0700

    update basetest api-keys

--- tests/basetest.py
+++ tests/basetest.py
@@ -1,17 +1,23 @@
 import pytest
 from simplifai import SimplifAIClient
 
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+ANTHROPIC_API_KEY = getenv("ANTHROPIC_API_KEY")
+OPENAI_API_KEY = getenv("OPENAI_API_KEY")
 
 def base_test_all_providers(func):
     return pytest.mark.parametrize("provider, client_kwargs, func_kwargs", [
     (
         "anthropic", 
-        {"api_key": "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"},
+        {"api_key": ANTHROPIC_API_KEY},
         {}
         ),
     (
         "openai", 
-        {"api_key": "sk-proj-JLo9QCF0Xrwu1s8X66fUT3BlbkFJ8y0422BoUjUgvWD1G6Qo"},
+        {"api_key": OPENAI_API_KEY},
         {}
     ),
     (
@@ -20,15 +26,4 @@ def base_test_all_providers(func):
         {"keep_alive": "10m", 'model': 'llama3.1-8b-num_ctx-8192:latest'}
     ),
     ])(func)
-    # @pytest.mark.parametrize("provider, client_kwargs", [
-    # # ("anthropic", {"api_key": "test"}),
-    # ("openai", {"api_key": "sk-proj-JLo9QCF0Xrwu1s8X66fUT3BlbkFJ8y0422BoUjUgvWD1G6Qo"}),
-    # ("ollama", {"host": "http://librem-2.local:11434"}),
-    # ])
-    # def wrapper(provider, client_kwargs, *args, **kwargs):
-    #     ai = SimplifAI({
-    #         provider: client_kwargs
-    #     })
-    #     return func(ai, provider, client_kwargs, *args, **kwargs)
-    # return wrapper
 
