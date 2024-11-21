70ba191cc34a41becbc5e461ad43169dcdda035e
Lucas Faudman <Lucasfaudman@gmail.com>
2024-11-20 20:34:16 -0800

    add tokenizers

---
+++ src/unifai/components/tokenizers/huggingface_tokenizer.py
@@ -0,0 +1,70 @@
+from typing import TYPE_CHECKING, Type, Optional, Sequence, Any, Union, Literal, TypeVar, ClassVar, Iterable,  Callable, Iterator, Iterable, Generator, Self, AbstractSet, Collection
+
+from transformers import AutoTokenizer, PreTrainedTokenizerBase
+from ...exceptions import UnifAIError, UnknownUnifAIError, TokenizerVocabError, TokenizerDisallowedSpecialTokenError
+from ._base_tokenizer import TokenizerAdapter
+
+T = TypeVar("T")
+
+class HuggingFaceTokenizer(TokenizerAdapter):
+    provider = "huggingface"
+
+    default_model = "bert-base-cased"
+    default_allowed_special: Literal["all"]|AbstractSet[str] = set()
+    default_disallowed_special: Literal["all"]|Collection[str] = "all"
+
+    _cache: ClassVar[dict[str, PreTrainedTokenizerBase]] = {}
+    
+    def import_client(self):
+        return self.lazy_import("transformers.AutoTokenizer")
+
+    def init_client(self, **client_kwargs):
+        self.client_kwargs.update(client_kwargs)
+        self._client = self.import_client()
+        return self._client
+
+    # def convert_exception(self, exception: Exception) -> UnifAIError:
+    #     if isinstance(exception, ValueError) and exception.args and "disallowed special token" in (message := exception.args[0]):
+    #         return TokenizerDisallowedSpecialTokenError(message=message, original_exception=exception)
+    #     if isinstance(exception, KeyError) and exception.args and "Unknown encoding" in (message := exception.args[0]):
+    #         return TokenizerVocabError(message=message, original_exception=exception)
+    #     return UnknownUnifAIError(message=str(exception), original_exception=exception)
+
+    def get_autotokenizer(self, model: Optional[str] = None, **kwargs) -> PreTrainedTokenizerBase:
+        model = model or self.default_model
+        if not (hf_tokenizer := self._cache.get(model)):
+            hf_tokenizer = self.client.from_pretrained(model, **{**self.client_kwargs, **kwargs})                
+            self._cache[model] = hf_tokenizer
+        return hf_tokenizer
+        
+    def _encode(
+            self, 
+            text: str, 
+            model: Optional[str] = None,                    
+            **kwargs
+    ) -> list[int]:
+        if "add_special_tokens" not in kwargs:
+            kwargs["add_special_tokens"] = False           
+        return self.get_autotokenizer(model).encode(text, **kwargs)
+    
+    def _decode(
+            self,
+            tokens: list[int],
+            model: Optional[str] = None,
+            **kwargs
+    ) -> str:
+        if "clean_up_tokenization_spaces" not in kwargs:
+            kwargs["clean_up_tokenization_spaces"] = True
+        if "skip_special_tokens" not in kwargs:
+            kwargs["skip_special_tokens"] = True
+        return self.get_autotokenizer(model).decode(tokens, **kwargs)
+
+    def _tokenize(
+            self,
+            text: str,
+            model: Optional[str] = None,
+            **kwargs
+    ) -> list[str]:
+        if "add_special_tokens" not in kwargs:
+            kwargs["add_special_tokens"] = False    
+        return self.get_autotokenizer(model).tokenize(text, **kwargs)
---
+++ src/unifai/components/tokenizers/tiktoken_tokenizer.py
@@ -0,0 +1,85 @@
+from typing import TYPE_CHECKING, Type, Optional, Sequence, Any, Union, Literal, TypeVar, ClassVar, Iterable,  Callable, Iterator, Iterable, Generator, Self, AbstractSet, Collection
+
+# if TYPE_CHECKING:
+#     import tiktoken
+import tiktoken
+
+from ...exceptions import UnifAIError, UnknownUnifAIError, TokenizerVocabError, TokenizerDisallowedSpecialTokenError
+from .._base_component import convert_exceptions
+from ._base_tokenizer import TokenizerAdapter
+
+T = TypeVar("T")
+
+class TikTokenTokenizer(TokenizerAdapter):
+    provider = "tiktoken"
+
+    default_encoding = "gpt2"
+    default_allowed_special: Literal["all"]|AbstractSet[str] = set()
+    default_disallowed_special: Literal["all"]|Collection[str] = "all"
+
+    _encoding_cache: ClassVar[dict[str, tiktoken.Encoding]] = {}
+
+    def import_client(self):
+        return self.lazy_import("tiktoken")
+
+    def init_client(self, **client_kwargs):
+        if (allowed_special := client_kwargs.get("allowed_special") or client_kwargs.pop("default_allowed_special", None)) is not None:
+            self.default_allowed_special = allowed_special
+        if (disallowed_special := client_kwargs.get("disallowed_special") or client_kwargs.pop("default_disallowed_special", None)) is not None:
+            self.default_disallowed_special = disallowed_special
+        self.client_kwargs.update(client_kwargs)
+        self._client = self.import_client()
+        return self._client
+    
+    def convert_exception(self, exception: Exception) -> UnifAIError:
+        if isinstance(exception, ValueError) and exception.args and "disallowed special token" in (message := exception.args[0]):
+            return TokenizerDisallowedSpecialTokenError(message=message, original_exception=exception)
+        if isinstance(exception, KeyError) and exception.args and "Unknown encoding" in (message := exception.args[0]):
+            return TokenizerVocabError(message=message, original_exception=exception)
+        return UnknownUnifAIError(message=str(exception), original_exception=exception)
+
+    def get_tiktoken_encoding(self, model: Optional[str] = None, encoding: Optional[str] = None) -> tiktoken.Encoding:
+        if model:
+            encoding = tiktoken.encoding_name_for_model(model)
+        if not encoding:
+            encoding = self.default_encoding
+        if not (tiktoken_encoding := self._encoding_cache.get(encoding)):
+            tiktoken_encoding = tiktoken.get_encoding(encoding)
+            self._encoding_cache[encoding] = tiktoken_encoding
+        return tiktoken_encoding
+    
+    def _add_default_specials(self, kwargs: dict[str, Any]) -> dict[str, Any]:
+        if "allowed_special" not in kwargs:
+            kwargs["allowed_special"] = self.default_allowed_special
+        if "disallowed_special" not in kwargs:
+            kwargs["disallowed_special"] = self.default_disallowed_special
+        return kwargs
+    
+    def _encode(
+            self, 
+            text: str, 
+            model: Optional[str] = None,                    
+            encoding: Optional[str] = None,
+            **kwargs
+    ) -> list[int]:
+        return self.get_tiktoken_encoding(model, encoding).encode(text, **self._add_default_specials(kwargs))
+    
+    def _decode(
+            self,
+            tokens: list[int],
+            model: Optional[str] = None,
+            encoding: Optional[str] = None,
+            **kwargs
+    ) -> str:
+        return self.get_tiktoken_encoding(model, encoding).decode(tokens, **kwargs)
+
+    def _tokenize(
+            self,
+            text: str,
+            model: Optional[str] = None,
+            encoding: Optional[str] = None,
+            **kwargs
+    ) -> list[str]:
+        tiktoken_encoding = self.get_tiktoken_encoding(model, encoding)
+        tokens = tiktoken_encoding.encode(text, **self._add_default_specials(kwargs))
+        return [tiktoken_encoding.decode_single_token_bytes(token).decode() for token in tokens]
