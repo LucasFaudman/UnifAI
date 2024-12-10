d8d88b5fba96c121a2f1dd037454c43f4855762c
Lucas Faudman <Lucasfaudman@gmail.com>
2024-12-10 03:44:03 -0800

    update toplevel embed interface for testing. other client TODO

--- src/unifai/client/_embed_client.py
+++ src/unifai/client/_embed_client.py
@@ -3,6 +3,7 @@ from typing import Any, Callable, Collection, Literal, Optional, Sequence, Type,
 
 if TYPE_CHECKING:
     from ..types.annotations import ComponentName, ProviderName
+    from ..types.documents import Document, Documents
     from ..components._base_components._base_embedder import Embedder, Embeddings
     from ..configs.embedder_config import EmbedderConfig
 
@@ -22,8 +23,7 @@ class UnifAIEmbedClient(BaseClient):
     def embed(
         self,            
         input: str | list[str],
-        provider: str = "default", 
-        config_or_name: "EmbedderConfig | ComponentName" = "default", 
+        provider_config_or_name: "ProviderName | EmbedderConfig | tuple[ProviderName, ComponentName]" = "default",
         model: Optional[str] = None,        
         dimensions: Optional[int] = None,
         task_type: Optional[Literal[
@@ -42,7 +42,7 @@ class UnifAIEmbedClient(BaseClient):
         **kwargs
         ) -> "Embeddings":
         
-        return self.embedder(provider, config_or_name).embed(
+        return self.embedder(provider_config_or_name).embed(
             input,
             model=model,
             dimensions=dimensions,
@@ -50,5 +50,39 @@ class UnifAIEmbedClient(BaseClient):
             truncate=truncate,
             reduce_dimensions=reduce_dimensions,
             use_closest_supported_task_type=use_closest_supported_task_type,
+            **kwargs
+        )
+
+    def embed_documents(
+            self,            
+            documents: Iterable["Document"],
+            provider_config_or_name: "ProviderName | EmbedderConfig | tuple[ProviderName, ComponentName]" = "default",
+            model: Optional[str] = None,
+            dimensions: Optional[int] = None,
+            task_type: Optional[Literal[
+                "retrieval_document", 
+                "retrieval_query", 
+                "semantic_similarity", 
+                "classification", 
+                "clustering", 
+                "question_answering", 
+                "fact_verification", 
+                "code_retrieval_query", 
+                "image"]] = None,
+            truncate: Literal[False, "end", "start"] = False,
+            reduce_dimensions: bool = False,
+            use_closest_supported_task_type: bool = True,  
+            batch_size: Optional[int] = None,           
+            **kwargs
+            ) -> "Documents":
+        return self.embedder(provider_config_or_name).embed_documents(
+            documents,
+            model=model,
+            dimensions=dimensions,
+            task_type=task_type,
+            truncate=truncate,
+            reduce_dimensions=reduce_dimensions,
+            use_closest_supported_task_type=use_closest_supported_task_type,
+            batch_size=batch_size,
             **kwargs
         )
