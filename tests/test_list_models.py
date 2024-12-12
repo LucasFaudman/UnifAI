e118c195228d675158a382711e6750d333ec0b89
Lucas Faudman <Lucasfaudman@gmail.com>
2024-12-11 20:54:00 -0800

    add list_llm_models at client level to replace old list_models

--- tests/test_list_models.py
+++ tests/test_list_models.py
@@ -1,12 +1,12 @@
 import pytest
 from unifai import UnifAI
-from basetest import base_test_llms
+from basetest import base_test_llms, API_KEYS
 
 @base_test_llms
 def test_list_models(provider, init_kwargs):
-    ai = UnifAI(api_keys=API_KEYS, provider_configs={provider: init_kwargs})
+    ai = UnifAI(api_keys=API_KEYS)
     for provider_arg in [provider, None]:
-        models = ai.list_models(provider_arg)
+        models = ai.list_llm_models(provider_arg)
         assert models
         assert isinstance(models, list)
         assert isinstance(models[0], str)
