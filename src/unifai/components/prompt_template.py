148d3bed8c1eeb6cc538dd7bd513d46e5825934c
Lucas Faudman <Lucasfaudman@gmail.com>
2024-12-11 22:16:49 -0800

    use combine_dicts

--- src/unifai/components/prompt_template.py
+++ src/unifai/components/prompt_template.py
@@ -1,19 +1,20 @@
 from typing import Any, Callable, Collection, Literal, Optional, Sequence, Type, Union, Self, Iterable, Mapping, Generator
 from pydantic import BaseModel
+from ..utils import combine_dicts
 
 class PromptTemplate(BaseModel):
     template: str|Callable[..., str] = "{content}"
-    value_formatters: Optional[Mapping[str|type, Optional[Callable[..., Any]]]] = None
-    nested_kwargs: Optional[Mapping[str, Any]] = None
-    template_getter_kwargs: Optional[Mapping[str, Any]] = None
-    kwargs: Optional[Mapping[str, Any]] = None
+    value_formatters: Optional[dict[str|type, Optional[Callable[..., Any]]]] = None
+    nested_kwargs: Optional[dict[str, Any]] = None
+    template_getter_kwargs: Optional[dict[str, Any]] = None
+    kwargs: Optional[dict[str, Any]] = None
 
 
     def __init__(self, 
                  template: str|Callable[..., str], 
-                 value_formatters: Optional[Mapping[str|type, Callable[..., Any]]] = None,                                
-                 nested_kwargs: Optional[Mapping[str, Any]] = None,
-                 template_getter_kwargs: Optional[Mapping[str, Any]] = None,                 
+                 value_formatters: Optional[dict[str|type, Callable[..., Any]]] = None,                                
+                 nested_kwargs: Optional[dict[str, Any]] = None,
+                 template_getter_kwargs: Optional[dict[str, Any]] = None,                 
                  **kwargs
                  ):
         
@@ -28,50 +29,29 @@ class PromptTemplate(BaseModel):
                            kwargs=kwargs
                            )
 
-
-    def resolve_kwargs(self, 
-                       nested_kwargs: Optional[Mapping[str, Any]] = None,                       
+    def resolve_kwargs(self,
+                       nested_kwargs: Optional[dict[str, Any]] = None,
                        **kwargs,
-                       ) -> dict[str, Any]:
-        
-        # Comhine nested_kwargs passed on init and nested_kwargs passed on format
-        resolved_nested_kwargs = {**self.nested_kwargs} if self.nested_kwargs else {}
-        if nested_kwargs:
-            resolved_nested_kwargs.update(nested_kwargs)
-        # Same as above but for kwargs
-        resolved_kwargs = {**self.kwargs} if self.kwargs else {}
-        if kwargs:
-            resolved_kwargs.update(kwargs)
-
+                       ) -> dict[str, Any]:        
+        # Combine kwargs/nested_kwargs passed on init with nested_kwargs passed on format
+        resolved_nested_kwargs = combine_dicts(self.nested_kwargs, nested_kwargs)
+        resolved_kwargs = combine_dicts(self.kwargs, kwargs)
         for key, value in resolved_kwargs.items():
             if callable(value):
-                resolved_kwargs[key] = value(**resolved_nested_kwargs.get(key, {}))
-        
+                resolved_kwargs[key] = value(**resolved_nested_kwargs.get(key, {}))      
         return resolved_kwargs
     
-
     def resolve_template(self,
                          template: Callable[..., str],
-                         template_getter_kwargs: Optional[Mapping[str, Any]] = None,
+                         template_getter_kwargs: Optional[dict[str, Any]] = None,
                          ) -> str:
-        
-        # Comhine template_getter_kwargs passed on init and template_getter_kwargs passed on format
-        resolved_template_getter_kwargs = {**self.template_getter_kwargs} if self.template_getter_kwargs else {}
-        if template_getter_kwargs:
-            resolved_template_getter_kwargs.update(template_getter_kwargs)
-
-        return template(**resolved_template_getter_kwargs)
-
+        return template(**combine_dicts(self.template_getter_kwargs, template_getter_kwargs))
 
     def format_values(self, 
                       resolved_kwargs: dict[str, Any],
-                      value_formatters: Optional[Mapping[str|type, Callable[..., Any]]] = None,                                      
-                      ):
-        
-        resolved_value_formatters = {**self.value_formatters} if self.value_formatters else {}
-        if value_formatters:
-            resolved_value_formatters.update(value_formatters)
-        
+                      value_formatters: Optional[dict[str|type, Callable[..., Any]]] = None,                                      
+                      ):        
+        resolved_value_formatters = combine_dicts(self.value_formatters, value_formatters)        
         global_formatter = resolved_value_formatters.get("*")
         for key, value in resolved_kwargs.items():       
             if key_formatter := resolved_value_formatters.get(key):
@@ -94,11 +74,10 @@ class PromptTemplate(BaseModel):
             
         return resolved_kwargs
     
-
     def format(self, 
-               nested_kwargs: Optional[Mapping[str, Any]] = None,
-               template_getter_kwargs: Optional[Mapping[str, Any]] = None,
-               value_formatters: Optional[Mapping[str|type, Callable[..., Any]]] = None,               
+               nested_kwargs: Optional[dict[str, Any]] = None,
+               template_getter_kwargs: Optional[dict[str, Any]] = None,
+               value_formatters: Optional[dict[str|type, Callable[..., Any]]] = None,               
                **kwargs,               
                ):
         
@@ -111,20 +90,18 @@ class PromptTemplate(BaseModel):
         resolved_kwargs = self.format_values(resolved_kwargs, value_formatters)
         return template_str.format(**resolved_kwargs)
     
-
     def __call__(self,
-                    nested_kwargs: Optional[Mapping[str, Any]] = None,
-                    template_getter_kwargs: Optional[Mapping[str, Any]] = None,
-                    value_formatters: Optional[Mapping[str|type, Callable[..., Any]]] = None,                 
+                    nested_kwargs: Optional[dict[str, Any]] = None,
+                    template_getter_kwargs: Optional[dict[str, Any]] = None,
+                    value_formatters: Optional[dict[str|type, Callable[..., Any]]] = None,                 
                     **kwargs,
                     ):
             return self.format(nested_kwargs, template_getter_kwargs, value_formatters, **kwargs)
 
-
     def __str__(self):
         return self.format()
-    
 
+    
 if __name__ == "__main__":
     hello = PromptTemplate(template="Hello {name}")
     print(hello(name="World")) # Prints Hello World
