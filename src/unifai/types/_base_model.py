799dc2597d110c6e4a301cc2849839996f171e18
Lucas Faudman <Lucasfaudman@gmail.com>
2025-02-09 19:19:27 -0800

    handle Strenum and Intenum

--- src/unifai/types/_base_model.py
+++ src/unifai/types/_base_model.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel as PydanticBaseModel, Field, ConfigDict, RootModel
 
 class BaseModel(PydanticBaseModel):
-    model_config = ConfigDict(use_attribute_docstrings=True)
+    model_config = ConfigDict(use_attribute_docstrings=True, use_enum_values=True)
 
